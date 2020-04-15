66a4a744db735f586888b65c9b789bed9e17063d
Adam Boda <adam.boda@mbp-aboda.home>
2020-04-15 19:36:58 +0100

    frontend commit

---
+++ frontend/src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Spicy!</h1>
+        <p>piquant palace</p>
+      </div>
+    );
+  }
+}
+
+export default Titles;
